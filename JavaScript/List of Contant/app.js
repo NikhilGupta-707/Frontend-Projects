f5a46493bbfab10e78d3fc2ab0da64a0aa2941a4
Nikhil Kumar <nikhilgupta108gm@gmail.com>
2026-03-14 02:21:47 +0530

    Second Js Project Using Variables

---
+++ JavaScript/List of Contant/app.js
@@ -0,0 +1,26 @@
+let Name1;
+let Name2;
+let Name3;
+
+Name1 = "Maxwell Wright";
+Name2 = "Raja Villarreal";
+Name3 = "Helen Richards";
+
+let Phone1
+let Phone2
+let phone3
+
+phone1 = "(0191) 719 6495";
+phone2 = "(0191) 719 6495";
+phone3 = "0800 1111";
+
+let Email1;
+let Email2;
+let Email3;
+
+Email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+Email2 =  "posuere.vulputate@sed.com";
+Email3 = "libero@convallis.edu"
+
+console.log(Name1,phone1, Email1);
+console.log(Name3,phone3, Email3);
--- JavaScript/Play with the florist/app.js
+++ JavaScript/Play with the florist/app.js
@@ -1,10 +1,13 @@
+// Question 1.
+
+/*
 // variable declaration.
   // For price
   let rosePrice;
   let lilyPrice;
   let tulipPrice;
 
-  // For Number OF flowers.
+  // Declare number of flowers.
   let numOfRoses;
   let numOflilies;
   let numOFtulips;
@@ -29,6 +32,44 @@
   totalPrice = roseValue + lilyValue + tulipValue;
 
 // for Output
+  console.log("Rose – unit price:",rosePrice, "quantity:",numOfRoses, "value:",roseValue);
+  console.log("Lily – unit price:",lilyPrice, "quantity:",numOflilies, "value:",lilyValue);
+  console.log("Rose – unit price:",tulipPrice, "quantity:",numOFtulips, "value:",tulipValue);
+  console.log("Total:",totalPrice);
+*/
+
+
+
+  // for question number 2
+  const rosePrice = 8;
+  const lilyPrice = 10;
+  const tulipPrice = 2;
+
+  let numOfRoses;
+  let numOflilies;
+  let numOFtulips;
+
+  numOfRoses = 70;
+  numOflilies = 50;
+  numOFtulips = 120;
+
+  let roseValue;
+  let lilyValue;
+  let tulipValue;
+
+
+
+
+  numOfRoses = 70 - 20;  // decrease by 20
+  numOflilies = 50 - 30; // decrease by 30
+
+  roseValue = rosePrice * numOfRoses;
+  lilyValue = lilyPrice * numOflilies;
+  tulipValue = tulipPrice * numOFtulips;
+
+  let totalPrice;
+  totalPrice = roseValue + lilyValue + tulipValue;
+
   console.log("Rose – unit price:",rosePrice, "quantity:",numOfRoses, "value:",roseValue);
   console.log("Lily – unit price:",lilyPrice, "quantity:",numOflilies, "value:",lilyValue);
   console.log("Rose – unit price:",tulipPrice, "quantity:",numOFtulips, "value:",tulipValue);
